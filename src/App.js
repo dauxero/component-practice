15d721af39b9810ec3d73759327aab4ee1abe7e5
carlos alberto gonzalez <carlos.gonzalez923@gmail.com>
2024-05-21 20:58:11 -0500

    creacion de tres componentes, agregaciones de envio de propiedades y uso de recorrido de arreglos y llaves unica

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import { MyComponent } from './components/MyComponent';
-import { SecondComponent } from './components/SecondComponent';
-import { ThirdComponent } from './components/ThirdComponent';
+import logo from "./logo.svg";
+import "./App.css";
+import { MyComponent } from "./components/MyComponent";
+import { SecondComponent } from "./components/SecondComponent";
+import { ThirdComponent } from "./components/ThirdComponent";
 
 function App() {
-
   const medicalRecord = {
     height: "160",
     bloodGroup: "RhO+",
-    allergies: "None"
-  }
+    allergies: "None",
+    dad: "dasd",
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Estructura inicial del proyecto y limpia.
-        </p>
+        <p>Estructura inicial del proyecto y limpia.</p>
         <SecondComponent />
-        <ThirdComponent 
-          name="Inés"
-          lastName="Oliveros"
-          card={medicalRecord}
-        />
+        <ThirdComponent name="Inés" lastName="Oliveros" card={medicalRecord} />
         <MyComponent />
       </header>
     </div>
